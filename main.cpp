d38630547d3b8481ff91b8a6c52e98d97b123b85
juanvergara3 <juan.vergara3@udea.edu.co>
2020-10-17 15:42:13 -0500

    Primeras concepciones del programa
    ultimo comit de la fase de analisis

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {
     float d, h_o, h_d; //parametros fijos
-    float x_0d, y_0d, v_0d, t_0d, t_fd; //parametros del proyectil defensico
 
     while(true){
         cout<<"Ingrese la distancia entre los cañones: "; cin>>d;
@@ -16,9 +15,11 @@ int main()
     cout<<"Ingrese la altura del cañon ofensivo: "; cin>>h_o;
     cout<<"Ingrese la altura del cañon defensivo: "; cin>>h_d;
 
+    float x_0o = 0, y_0o = h_o, v_0o = 0, t_0o = 0, t_fo = 0, a_o = 0, r_impacto = ; //parametros del proyectil defensivo
+    float x_0d = d, y_0d = h_d, v_0d = 0, t_0d = t_0o+2, t_fd = 0, a_d; //parametros del proyectil defensico
 
-
-
+    float proyectil_o[2] = {v_0o, a_o};
+    float proyectil_d[2] = {v_0d, a_d};
 
 
 
